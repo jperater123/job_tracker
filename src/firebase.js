294bceed729b321e243cfb416473554014b38efc
jperater123 <jasperperater@gmail.com>
2024-10-25 11:35:17 +0800

    env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,22 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+const apiKey = process.env.REACT_APP_apiKey;
+const authDomain = process.env.REACT_APP_authDomain;
+const projectId = process.env.REACT_APP_projectId;
+const storageBucket = process.env.REACT_APP_storageBucket;
+const messagingSenderId = process.env.REACT_APP_messagingSenderId;
+const appId = process.env.REACT_APP_appId;
+
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJRtNoMMfe4wN1g98ZCS-vjSFiAr70-6Y",
-    authDomain: "job-tracking-db2c8.firebaseapp.com",
-    projectId: "job-tracking-db2c8",
-    storageBucket: "job-tracking-db2c8.appspot.com",
-    messagingSenderId: "462958444538",
-    appId: "1:462958444538:web:f6a637a5488302e7012e43"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
   };
 
 
